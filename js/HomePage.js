9c010ebf67fa6cbc6b99a7bc269e139f96521ec0
dangtiendat288 <33756043+dangtiendat288@users.noreply.github.com>
2022-04-14 12:21:31 -0700

    change file paths

--- js/HomePage.js
+++ js/HomePage.js
@@ -40,7 +40,7 @@ function setup() {
       this.color = "#c09e82";
     }
     game1Btn.onPress = function () {
-      window.location.replace('/game1.html');
+      window.location.replace('./game1.html');
     }
 
     //Create game2Btn  
@@ -62,7 +62,7 @@ function setup() {
       this.color = "#c09e82";
     }
     game2Btn.onPress = function () {
-      window.location.replace('/game2.html');
+      window.location.replace('./game2.html');
     }
 
     //Create game3Btn  
@@ -84,7 +84,7 @@ function setup() {
       this.color = "#c09e82";
     }
     game3Btn.onPress = function () {
-      window.location.replace('/game3.html');
+      window.location.replace('./game3.html');
     }
 }
 
